d8ca77a7486c1c9214a784d92ffbbbc46d49f5a5
kalizink <kalizink65@gmail.com>
2024-02-14 10:22:19 -0600

    Added NavbarComp

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import NavbarComp from "./NavbarComp.js";
 
 export default function App() {
-	return <div>it worked</div>;
+	return (
+		<div className="App">
+			<NavbarComp />
+			<h1 className="text-center">Let's Grow 🌸</h1>
+		</div>
+	);
 }
---
+++ src/components/PlantsPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PlantsPage() {
+	return (
+		<div className="PlantsPage">
+			<h1>Plants Page</h1>
+		</div>
+	);
+}
