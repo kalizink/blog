bdb9dcbc30545c5eb625333826703b1843d0b476
kalizink <kalizink65@gmail.com>
2024-02-14 11:16:02 -0600

    Added img to PlantPage

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ export default function App() {
 	return (
 		<div>
 			<NavbarComp />
+			<br />
 			<div className="App">
 				<h1 className="text-center">Let's Grow 🌸</h1>
 			</div>
--- src/components/PlantsPage.js
+++ src/components/PlantsPage.js
@@ -1,12 +1,31 @@
 import React from "react";
 import NavbarComp from "./NavbarComp";
+import Feb2 from "../images/2:2.png";
+import "./PlantsPage.css";
 
 export default function PlantsPage() {
 	return (
 		<div>
 			<NavbarComp />
+			<br />
 			<div className="PlantsPage mt-5">
 				<h1 className="text-center">Plants Page</h1>
+				<div className="plant-container">
+					{/*new day */}
+					<div className="row">
+						<div className="column">
+							<h2 className="text-center">Day 1: Feb 2nd 2024</h2>
+						</div>
+						<div className="column">
+							<img
+								src={Feb2}
+								alt="no-rel"
+								title="Hyancinth"
+								className="img-fluid"
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
